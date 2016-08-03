3d81abe6e05dbbf48ab5c3d95cf321d7dbff63de
Kayvan Mazaheri <KayvanMazaheri@GMail.com>
2016-08-03 14:00:24 +0430

    fixed async tasks bugs

--- gulpfile.js
+++ gulpfile.js
@@ -15,31 +15,35 @@ gulp.task('clean:temp', function() {
 	return del(['temp/**/*']);
 });
 
-gulp.task('clone:repo', ['clean:temp'], function() {
-	return git.clone(DataRepo, {args: './temp/repo'}, function(err) {
-		throw err;
+gulp.task('clone:repo', ['clean:temp'], function(done) {
+	git.clone(DataRepo, {args: './temp/repo'}, function(err) {
+		if(err)
+			throw err;
+		done();
 	});
 });
 
 var prepareData = function(content) {
-	var original = content;
+	var original = JSON.parse(content);
 	var result = {};
+
 	for (var i = 0; i < original.length; i++) {
 		var province = original[i].name;
 		result[province] = [];
-		for (var j = 0; j < original[i].cities.length; j++) {
-			var city = original[i].cities[j].name;
+		var cities = original[i].cities;
+		for (var j = 0; j < cities.length; j++) {
+			var city = cities[j].name;
 			result[province].push(city);
 		}
 	}
-	return result;
+	return JSON.stringify(result);
 };
 
 gulp.task('prepare:data', ['clone:repo'], function() {
 	return gulp.src('temp/repo/*.json')
 		.pipe(change(prepareData))
 		.pipe(rename({basename:'data'}))
-		.pipe(gulp.dest('temp'));
+		.pipe(gulp.dest('temp/'));
 });
 
 gulp.task('add:data', ['prepare:data'], function() {
@@ -48,10 +52,10 @@ gulp.task('add:data', ['prepare:data'], function() {
 	if(!data)
 		throw 'data.json is not available';
 
-	var line = 'var data = ' + data + ';'
+	var line = 'var data = ' + JSON.stringify(data) + ';\n'
 	
 	return gulp.src('index.js')
-		.pipe(rename{basename:'ir-city-select'})
+		.pipe(rename({basename:'ir-city-select'}))
 		.pipe(insert.prepend(line))
 		.pipe(gulp.dest('dist/'))
 		.pipe(uglify())
@@ -60,4 +64,4 @@ gulp.task('add:data', ['prepare:data'], function() {
 });
 
 
-gulp.task('default', ['clean:temp', 'clone:repo', 'prepare:data', 'add:data']);
+gulp.task('default', ['add:data']);
