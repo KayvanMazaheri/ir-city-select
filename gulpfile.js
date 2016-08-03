086c9702da18fdb43ba8b6e63b456060e002a522
Kayvan Mazaheri <KayvanMazaheri@GMail.com>
2016-08-03 12:42:28 +0430

    convert original json data array to single object

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,42 @@
+const DataRepo = 'git@github.com:pesarkhobeee/iran-states-and-cities-json-and-sql-including-area-coordinations.git';
+const DataFile = 'iran_cities_with_coordinates.json';
+
 var gulp = require('gulp');
+var git = require('gulp-git');
+var data = require('gulp-data');
+var del = require('del');
+var handlebars = require('gulp-handlebars');
+var insert = require('gulp-insert');
+var change = require('gulp-change');
+
+gulp.task('clean:temp', function() {
+	return del(['temp/**/*']);
+});
+
+gulp.task('clone:repo', function() {
+	return git.clone(DataRepo, {args: './temp/repo'}, function(err) {
+		throw err;
+	});
+});
+
+var prepareData = function(content) {
+	var original = content;
+	var result = {};
+	for (var i = 0; i < original.length; i++) {
+		var province = original[i].name;
+		result[province] = [];
+		for (var j = 0; j < original[i].cities.length; j++) {
+			var city = original[i].cities[j].name;
+			result[province].push(city);
+		}
+	}
+	return result;
+};
+
+gulp.task('prepare:data', function() {
+	return 
+});
+
 
 gulp.task('default', function() {
 	
